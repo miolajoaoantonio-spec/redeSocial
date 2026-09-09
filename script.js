aec8ed82715d8d50f1301a6e96123cf610a16f89
miolajoaoantonio-spec <miolajoaoantonio@gmail.com>
2026-09-09 11:05:22 -0300

    ok

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventLisrtener("DOMContentLoaded", ()=>{
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media");
+    if (!likeBtn) return;
+
+    const likesCountSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector("bookmark-Btn");
+    
+    let isLiked = false;
+    let baseLikes = 0;
+    
+
+})
